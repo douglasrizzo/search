ce53c09f7a8fc99b4dde45dcd8766f1cb07a1fc4
Douglas <douglasrizzom@gmail.com>
2017-06-27 01:47:09 -0300

    implemented a new Heuristic, yet to be tested

--- include/Heuristic.hpp
+++ include/Heuristic.hpp
@@ -9,12 +9,12 @@
 
 class Heuristic {
 public:
-    virtual int calc(const GameState currentState)=0;
+    virtual int calc(GameState &currentState)=0;
 };
 
 class Manhattan : public Heuristic {
 public:
-    int calc(GameState currentState) {
+    int calc(GameState &currentState) {
         int distance = 0, dimension = currentState.getDimension();
         for (int i = 0; i < pow(dimension, 2); i++) {
             int *pos = currentState.find(i);
@@ -23,8 +23,24 @@ public:
             int x_opt = i != 0 ? (i - 1) / dimension : 2,
                     y_opt = i != 0 ? (i - 1) % dimension : 2;
 
-            string line = currentState.to_line_string();
             distance += abs(x - x_opt) + abs(y - y_opt);
+        }
+        return distance;
+    }
+};
+
+class TileDifference : public Heuristic {
+public:
+    int calc(GameState &currentState) {
+        int distance = 0, dimension = currentState.getDimension();
+        for (int i = 0; i < pow(dimension, 2); i++) {
+            int *pos = currentState.find(i);
+            int x = pos[0], y = pos[1];
+            delete[] pos;
+            int x_opt = i != 0 ? (i - 1) / dimension : 2,
+                    y_opt = i != 0 ? (i - 1) % dimension : 2;
+
+            distance += x != x_opt || y != y_opt;
         }
         return distance;
     }
