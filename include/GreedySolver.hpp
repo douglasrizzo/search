f3c30bd6ef0ca3bb79f2ececd324d76385d28207
Douglas <douglasrizzom@gmail.com>
2017-06-30 02:01:39 -0300

    changed GreedySolver name and created HillClimbingSolver

--- include/GreedySolver.hpp
+++ include/GreedySolver.hpp
@@ -2,22 +2,22 @@
 // Created by dodo on 24/06/17.
 //
 
-#ifndef BUSCA_BESTFIRSTSOLVER_HPP
-#define BUSCA_BESTFIRSTSOLVER_HPP
+#ifndef BUSCA_GREEDYSOLVER_HPP
+#define BUSCA_GREEDYSOLVER_HPP
 
 #include <OrderedList.hpp>
 #include "Solver.hpp"
 #include "Heuristic.hpp"
 
-class BestFirstSolver : public Solver {
+class GreedySolver : public Solver {
 private:
     Heuristic *heuristic;
 public:
-    explicit BestFirstSolver(Heuristic *h) {
+    explicit GreedySolver(Heuristic *h) {
         heuristic = h;
     }
 
-    ~BestFirstSolver() {
+    ~GreedySolver() {
         delete heuristic;
     }
 
@@ -33,14 +33,13 @@ public:
     LinkedList<GameState *> solve(Game &game, GameState &g0) {
         OrderedList<GameState *> expanded = OrderedList<GameState *>(compare);
 
-        const time_t start = time(NULL);
+        const clock_t start = clock();
         expanded.insert(&g0);
         while (!expanded.isEmpty()) {
             GameState *currentGame = expanded.remove(0);
 
             if (*game.getGoal() == *currentGame) {
-                secondsToSolve = difftime(time(NULL), start);
-                return resultSteps(currentGame);
+                return endSearch(currentGame, start);
             }
 
             LinkedList<GameState *> children = visit(currentGame);
@@ -54,4 +53,4 @@ public:
     }
 };
 
-#endif //BUSCA_BESTFIRSTSOLVER_HPP
+#endif
---
+++ include/HillClimbingSolver.hpp
@@ -0,0 +1,69 @@
+//
+// Created by dodo on 24/06/17.
+//
+
+#ifndef BUSCA_HILLCLINBINGSOLVER_HPP
+#define BUSCA_HILLCLINBINGSOLVER_HPP
+
+#include <OrderedList.hpp>
+#include "Solver.hpp"
+#include "Heuristic.hpp"
+
+class HillClimbingSolver : public Solver {
+private:
+    Heuristic *heuristic;
+    bool steepest;
+public:
+    explicit HillClimbingSolver(Heuristic *h) {
+        heuristic = h;
+        steepest = false;
+    }
+
+    explicit HillClimbingSolver(Heuristic *h, bool steepest) {
+        heuristic = h;
+        this->steepest = steepest;
+    }
+
+    ~HillClimbingSolver() {
+        delete heuristic;
+    }
+
+    static int compare(GameState *a, GameState *b) {
+        Manhattan h;
+        if (h.calc(*a) < h.calc(*b))
+            return 1;
+        if (h.calc(*a) > h.calc(*b))
+            return -1;
+        return 0;
+    }
+
+    LinkedList<GameState *> solve(Game &game, GameState &g0) {
+        GameState *only_one = &g0;
+        const clock_t start = clock();
+        while (true) {
+            if (*game.getGoal() == *only_one) {
+                return endSearch(only_one, start);
+            }
+
+            LinkedList<GameState *> children = visit(only_one);
+
+            bool changed = false;
+            while (!children.isEmpty()) {
+                GameState *child = children.remove(0);
+                if (compare(only_one, child) < 0) {
+                    changed = true;
+                    only_one = child;
+                    if (!steepest)
+                        break;
+                }
+            }
+
+            if (!changed) {
+                cout << "WARNING: hill climbing procedure reached a local minimum\n";
+                return endSearch(only_one, start);
+            }
+        }
+    }
+};
+
+#endif
