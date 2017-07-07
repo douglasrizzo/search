d5f2dfa226216af1dad75bdd0f55696f78269b77
Douglas <douglasrizzom@gmail.com>
2017-07-07 00:57:33 -0300

    added documentation to the tests

--- test/main.cpp
+++ test/main.cpp
@@ -8,6 +8,15 @@
 #include "GreedySolver.hpp"
 #include "HillClimbingSolver.hpp"
 
+int myRandom(int max) {
+  time_t result = time(nullptr);
+  mt19937_64 r;
+  r.seed((unsigned long) std::localtime(&result));
+  return abs((int) r() % max);
+}
+
+//! Reads an input file, transforming it into a list of strings
+//! \return the list of strings
 LinkedList<string> *getPuzzles() {
   LinkedList<string> *puzzles = new LinkedList<string>();
   string line;
@@ -22,6 +31,8 @@ LinkedList<string> *getPuzzles() {
   return puzzles;
 }
 
+//! Checks if the goal state is correctly generated
+//! \return 0 if success
 int testGoalCreation() {
   Game game = Game(3);
 
@@ -29,6 +40,8 @@ int testGoalCreation() {
   return 0;
 }
 
+//! Gets a puzzle and performs a series of actions in it to test transitions
+//! \return 1 if success
 int testStateTransitions() {
   GameState *gs = new GameState(getPuzzles()->get(0));
 
@@ -49,6 +62,8 @@ int testStateTransitions() {
   return 0;
 }
 
+//! Tests the solvability of the puzzles in the input file.
+//! Must output 1 for all of them.
 void testSolvability() {
   LinkedList<string> *l = getPuzzles();
 
@@ -56,37 +71,20 @@ void testSolvability() {
     cout << GameState(l->remove(0)).isSolvable() << endl;
 }
 
-void testFastSolvers() {
-  LinkedList<string> *l = getPuzzles();
-
-  Game g = Game(3);
-
-  while (! l->isEmpty()) {
-    GameState gs = GameState(l->remove(0));
-
-    AStarSolver s1 = AStarSolver(new Manhattan());
-    s1.solve(g, gs);
-    cout << s1.to_string() << endl;
-    GreedySolver s2 = GreedySolver(new Manhattan());
-    s2.solve(g, gs);
-    cout << s2.to_string() << endl;
-  }
-
-  delete l;
-}
-
+//! Tests the overloaded equality operator for GameState
 void testEquality() {
   GameState gs1 = GameState("1 2 3 4 5 6 7 8 0"),
       gs2 = GameState("1 2 3 4 5 6 7 8 0"),
       gs3 = GameState("0 1 3 2 4 8 6 7 5"),
       gs4 = GameState("0 1 3 2 4 8 6 7 5");
 
-  if (! (gs1 == gs1) or ! (gs2 == gs2) or ! (gs3 == gs3) or ! (gs4 == gs4) or ! (gs1 ==
-      gs2) or (gs1 == gs3) or (gs1 == gs4) or (gs2 == gs3) or (gs2 == gs4) or ! (gs3 == gs4)) {
+  if (gs1 != gs1 or gs2 != gs2 or gs3 != gs3 or gs4 != gs4 or gs1 != gs2 or gs3 != gs4 or
+      gs1 == gs3 or gs1 == gs4 or gs2 == gs3 or gs2 == gs4) {
     throw exception();
   }
 }
 
+//! Tests the generation of GameState objects from the input file strings
 int testStateFromFile() {
   LinkedList<string> *l = getPuzzles();
 
@@ -98,25 +96,68 @@ int testStateFromFile() {
   return 0;
 }
 
-int testSingleSolver(Solver *s, Game &g, GameState &gs) {
+//! Generic function to test a single solver and print its output
+int testSingleSolver(Solver *s, Game &g, GameState &gs, bool verbose) {
   LinkedList<GameState *> results = s->solve(g, gs);
 
-  while (! results.isEmpty())
-    cout << results.remove(results.getSize() - 1)->to_string() << endl;
+  if (verbose) {
+    while (! results.isEmpty())
+      cout << results.remove(results.getSize() - 1)->to_string() << endl;
 
-  cout << s->to_string() << endl;
+    cout << s->to_string() << endl;
+  }
 
   return 0;
 }
 
+//! Tests the fastest solvers against all of the puzzles.
+void testFastSolvers() {
+  LinkedList<string> *l = getPuzzles();
+
+  Game g = Game(3);
+
+  int n1, n2, n3, n4, totalPuzzles = l->getSize();
+  n1 = n2 = n3 = n4 = 0;
+
+  while (! l->isEmpty()) {
+    cout << l->getSize() << "/" << totalPuzzles << endl;
+    GameState gs = GameState(l->remove(myRandom(l->getSize())));
+
+    AStarSolver *s1 = new AStarSolver(new Manhattan());
+    GreedySolver *s2 = new GreedySolver(new Manhattan());
+    HillClimbingSolver *s3 = new HillClimbingSolver(new Manhattan(), false);
+    HillClimbingSolver *s4 = new HillClimbingSolver(new Manhattan(), true);
+
+    testSingleSolver(s1, g, gs, false);
+    testSingleSolver(s2, g, gs, false);
+    testSingleSolver(s3, g, gs, false);
+    testSingleSolver(s4, g, gs, false);
+
+    n1 += s1->isSolved();
+    n2 += s2->isSolved();
+    n3 += s3->isSolved();
+    n4 += s4->isSolved();
+
+    delete s1, s2, s3, s4;
+
+  }
+
+  cout << "                    A*: " << n1 << "/" << totalPuzzles
+       << "\n                Greedy: " << n2 << "/" << totalPuzzles
+       << "\n         Hill Climbing: " << n3 << "/" << totalPuzzles
+       << "\nSteepest Hill Climbing: " << n4 << "/" << totalPuzzles << endl;
+
+  delete l;
+}
+
+//! Creates one of each solver, randomly selects a puzzle from the
+//! input file and tests all sovers against it.
+//! Non-informed solvers might take too long to finish some states.
 int testAllSolvers() {
   LinkedList<string> *puzzles = getPuzzles();
-  time_t result = time(nullptr);
 
-  mt19937_64 myRandom;
-  myRandom.seed((unsigned long) std::localtime(&result));
   Game game = Game(3);
-  int chosen_one = abs((int) myRandom() % puzzles->getSize());
+  int chosen_one = myRandom(puzzles->getSize());
   GameState gs = GameState(puzzles->remove(chosen_one));
 
   delete puzzles;
@@ -128,23 +169,23 @@ int testAllSolvers() {
   BreadthFirstSolver *s5 = new BreadthFirstSolver();
   DepthFirstSolver *s6 = new DepthFirstSolver();
 
-  testSingleSolver(s1, game, gs);
-  testSingleSolver(s2, game, gs);
-  testSingleSolver(s3, game, gs);
-  testSingleSolver(s4, game, gs);
-  testSingleSolver(s5, game, gs);
-  testSingleSolver(s6, game, gs);
+  testSingleSolver(s1, game, gs, true);
+  testSingleSolver(s2, game, gs, true);
+  testSingleSolver(s3, game, gs, true);
+  testSingleSolver(s4, game, gs, true);
+  testSingleSolver(s5, game, gs, true);
+  testSingleSolver(s6, game, gs, true);
 
   return 0;
 }
 
 int main() {
-  testGoalCreation();
-  testStateFromFile();
-  testStateTransitions();
-  testEquality();
-  testSolvability();
+//  testGoalCreation();
+//  testStateFromFile();
+//  testStateTransitions();
+//  testEquality();
+//  testSolvability();
   testFastSolvers();
-  testAllSolvers();
+//  testAllSolvers();
   return 0;
 }
