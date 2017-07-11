0bf8f0f48a33b8222e3fc0d80467e722f10cd9a1
Douglas <douglasrizzom@gmail.com>
2017-07-10 21:40:52 -0300

    solver now has the option not to track visited nodes

--- include/Solver.hpp
+++ include/Solver.hpp
@@ -13,7 +13,7 @@
 #include "GameState.hpp"
 #include "Game.hpp"
 
-// !Base class for all 8-puzzle solvers.
+//!Base class for all 8-puzzle solvers.
 class Solver {
  private:
 
@@ -33,25 +33,27 @@ class Solver {
   // used in case the solver is incomplete
   bool solved;
 
-  // ! Checks whether a node has already been visited by the solver.
-  // ! \param g The state to look for
-  // ! \return true if g has been visited, otherwise false
+  //! Checks whether a node has already been visited by the solver.
+  //! \param g The state to look for
+  //! \return true if g has been visited, otherwise false
   bool isVisited(GameState &g) {
     return visited.contains(&g);
   }
 
-  // ! Visit a game state, adding it to the list of visited states and returning
-  // its valid child states.
-  // ! \param current the state to be visited
-  // ! \return a list of the valid child states. A valid child state is the
-  // result state of applying a valid action to the current state. Only states
-  // that have not been visited before by the solver are returned.
   LinkedList<GameState *> visit(GameState *current) {
-    LinkedList<GameState *> retornus;
+    return visit(current, true);
+  }
+
+  //! Visit a game state, adding it to the list of visited states and returning its valid child states.
+  //! \param current the state to be visited
+  //! \return a list of the valid child states. A valid child state is the result state of applying a valid action to the current state. Only states that have not been visited before by the solver are returned.
+  LinkedList<GameState *> visit(GameState *current, bool keepVisited) {
+    LinkedList<GameState *> children;
     GameAction actions[4]{RIGHT, DOWN, LEFT, UP};
 
     // add current state to the list of visited states
-    visited.insert(current);
+    if (keepVisited)
+      visited.insert(current);
 
     // keep a record of the maximum depth the solver has explored
     if (current->getDepth() > maxDepth) maxDepth = current->getDepth();
@@ -65,26 +67,26 @@ class Solver {
         // child node has not been visited by the solver before
         GameState *newState = new GameState(current, actions[i]);
 
-        if (! isVisited(*newState)) retornus.insert(newState);
+        if (! isVisited(*newState)) children.insert(newState);
         else delete newState;
       }
     }
 
     // throws an exception in case all actions give valid states,
     // since at least one state was already visited
-    if ((retornus.getSize() == 4) && (current->getDepth() > 0)) {
+    if (keepVisited && (children.getSize() == 4) && (current->getDepth() > 0)) {
       throw invalid_argument("Duplicate states being visited.");
     }
 
-    return retornus;
+    return children;
   }
 
-  // ! End the search, generating the steps from the initial state to the goal
+  //! End the search, generating the steps from the initial state to the goal
   // state.
-  // ! \param currentGame the state containing the goal
-  // ! \param start the time the solve began, in order to calculate its time
+  //! \param currentGame the state containing the goal
+  //! \param start the time the solve began, in order to calculate its time
   // span
-  // ! \return list containing the steps from the initial state to the goal
+  //! \return list containing the steps from the initial state to the goal
   // state
   LinkedList<GameState *> endSearch(GameState *currentGame, const clock_t start) {
     secondsToSolve = float(clock() - start) / CLOCKS_PER_SEC;
@@ -134,19 +136,16 @@ class Solver {
     solved = false;
   }
 
-  // ! Explores the game tree in search of the goal state.
-  // ! Exploration is done by applying one of the four valid actions of the
-  // 8-puzzle to intermediate,
-  // ! non-goal states until the goal state is reached.
-  // ! \param g A description of the game.
-  // ! \param gs The initial state of the game.
-  // ! \return list containing all states explored, from the initial state to
-  // the goal state.
-  virtual LinkedList<GameState *> solve(Game &g,
-                                        GameState &gs) = 0;
+  //! Explores the game tree in search of the goal state.
+  //! Exploration is done by applying one of the four valid actions of the 8-puzzle to intermediate,
+  //! non-goal states until the goal state is reached.
+  //! \param g A description of the game.
+  //! \param gs The initial state of the game.
+  //! \return list containing all states explored, from the initial state to the goal state.
+  virtual LinkedList<GameState *> solve(Game &g, GameState &gs) = 0;
 
-  // ! Generates a string containing useful information from the solver run.
-  // ! \return string representation of the state-space exploration
+  //! Generates a string containing useful information from the solver run.
+  //! \return string representation of the state-space exploration
   string to_string() {
     return string(
         "\t\t\t\tSeconds: ").append(std::to_string(getSecondsToSolve())).append(
